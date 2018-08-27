4f588c87e0e5eae9855a4f0d6a8a5694ffe5f04c
Jeremy Blalock <jeremyjblalock@gmail.com>
2018-08-27 12:40:40 -0700

    Adds #import <UIKit/UIKit.h>

--- ios/RNIcloudFilePicker.h
+++ ios/RNIcloudFilePicker.h
@@ -1,9 +1,10 @@
-
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
 #import <React/RCTBridgeModule.h>
 #endif
+
+#import <UIKit/UIKit.h>
 
 @interface RNIcloudFilePicker : NSObject <RCTBridgeModule, UIDocumentPickerDelegate>
 
